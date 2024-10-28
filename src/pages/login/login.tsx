1944ff9c71dbcc25f39b0d96cc86fd0345b47388
Jhn0510 <2097391824@qq.com>
2024-10-28 16:25:59 +0800

    tabbar 和路由配置

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from '@tarojs/components';
+
+export default function Login() {
+
+    return (
+        <View>
+            <Text>Hello world!</Text>
+        </View>
+    )
+}
